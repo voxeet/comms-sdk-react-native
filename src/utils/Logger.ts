e62b8515e8b8613e482dbff105eb311084e54dbe
Piotrek Polus <piotr.polus@miquido.com>
2021-09-09 14:49:02 +0200

    DEXA-19: Logger fix

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -4,7 +4,6 @@ export default class Logger {
   }
   static error(message: string) {
     console.log(`[ERROR]] DolbyIAPI SDK ${message}`);
-    throw new Error(message);
   }
   static warning(message: string) {
     console.log(`[WARNING]] DolbyIAPI SDK ${message}`);
