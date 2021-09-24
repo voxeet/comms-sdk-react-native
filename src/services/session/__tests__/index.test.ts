6e30a993a2428d67a93199bc5e4cadfc7f27bf1b
michal.gruszczak <michal.gruszczak@miquido.com>
2021-09-24 14:49:05 +0200

    DEXA-79: RN - add unit tests for Main Module, SessionService, ConferenceService

--- src/services/session/__tests__/index.test.ts
+++ src/services/session/__tests__/index.test.ts
@@ -1,1 +1,30 @@
-it.todo('write a test');
+import SessionService from '../SessionService';
+
+/** Mocking function */
+
+jest.mock('react-native', () => {
+  const RN = jest.requireActual('react-native');
+  RN.NativeModules.DolbyIoIAPISessionServiceModule = {
+    open: jest.fn(),
+    close: jest.fn(),
+  };
+  RN.NativeModules.DolbyIoIAPIModule = {};
+  return RN;
+});
+
+import { NativeModules } from 'react-native';
+const { DolbyIoIAPISessionServiceModule } = NativeModules;
+
+/** SessionService - "open" method test */
+
+test('SessionService - "open" method test', () => {
+  SessionService.open({});
+  expect(DolbyIoIAPISessionServiceModule.open).toHaveBeenCalledWith({});
+});
+
+/** SessionService - "close" method test */
+
+test('SessionService - "close" method test', () => {
+  SessionService.close();
+  expect(DolbyIoIAPISessionServiceModule.open).toHaveBeenCalled();
+});
