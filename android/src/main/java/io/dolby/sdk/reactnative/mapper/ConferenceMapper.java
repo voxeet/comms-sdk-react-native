068fdb738cba5670b0243146d8adfdcb1cbc7336
Agnieszka Adamczyk <agnieszka.adamczyk@miquido.com>
2021-09-29 15:49:21 +0200

    DEXA-43 Add more javadoc, notnull annotations, nullability checks, change participant mapping

--- android/src/main/java/io/dolby/sdk/reactnative/mapper/ConferenceMapper.java
+++ android/src/main/java/io/dolby/sdk/reactnative/mapper/ConferenceMapper.java
@@ -8,11 +8,14 @@ import com.voxeet.sdk.json.ConferencePermission;
 import com.voxeet.sdk.json.internal.ParamsHolder;
 import com.voxeet.sdk.models.Conference;
 import com.voxeet.sdk.models.Participant;
+import com.voxeet.sdk.services.conference.information.ConferenceStatus;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import io.dolby.sdk.reactnative.utils.RNCollectionExtractor;
 
@@ -53,7 +56,7 @@ public class ConferenceMapper {
     }
 
     @Nullable
-    public String toConferenceId(ReadableMap conference) {
+    public String toConferenceId(@NotNull ReadableMap conference) {
         if (rnCollectionExtractor.hasKey(conference, CONFERENCE_ID)) {
             return rnCollectionExtractor.getString(conference, CONFERENCE_ID);
         } else return null;
@@ -120,16 +123,42 @@ public class ConferenceMapper {
         map.putString(CONFERENCE_ID, conference.getId());
         map.putString(CONFERENCE_ALIAS, conference.getAlias());
         map.putBoolean(CONFERENCE_IS_NEW, conference.isNew());
-        map.putString(CONFERENCE_STATUS, conference.getState().name());
-        map.putMap(CONFERENCE_PARAMS, toParams(conference));
-        map.putArray(CONFERENCE_PERMISSIONS, toPermissions(conference));
-        map.putArray(CONFERENCE_PARTICIPANTS, toParticipants(conference));
+        map.putString(CONFERENCE_STATUS, toString(conference.getState()));
+        map.putMap(CONFERENCE_PARAMS, toParamsMap(conference));
+        map.putArray(CONFERENCE_PERMISSIONS, toPermissionsArray(conference.getPermissions()));
+        map.putArray(CONFERENCE_PARTICIPANTS, toParticipantsArray(conference.getParticipants()));
 
         return map;
     }
 
     @NotNull
-    private ReadableMap toParams(Conference conference) {
+    private String toString(@NotNull ConferenceStatus status) {
+        switch (status) {
+            case CREATED:
+                return "Created";
+            case DESTROYED:
+                return "Destroyed";
+            case ENDED:
+                return "Ended";
+            case ERROR:
+                return "Error";
+            case JOINED:
+                return "Joined";
+            case LEFT:
+                return "Left";
+            case DEFAULT:
+            case CREATING:
+            case JOINING:
+            case FIRST_PARTICIPANT:
+            case NO_MORE_PARTICIPANT:
+            case LEAVING:
+            default:
+                return "Unknown";
+        }
+    }
+
+    @NotNull
+    private ReadableMap toParamsMap(@NotNull Conference conference) {
         HashMap<String, Object> metadata = conference.getMetadata();
 
         WritableMap map = new WritableNativeMap();
@@ -157,19 +186,55 @@ public class ConferenceMapper {
     }
 
     @NotNull
-    private WritableNativeArray toPermissions(@NotNull Conference conference) {
+    private WritableNativeArray toPermissionsArray(@NotNull Set<ConferencePermission> permissions) {
         WritableNativeArray permissionsArray = new WritableNativeArray();
-        for (ConferencePermission permission : conference.getPermissions()) {
-            permissionsArray.pushString(permission.name());
+        for (ConferencePermission permission : permissions) {
+            if (permission != null) {
+                permissionsArray.pushString(toString(permission));
+            }
         }
         return permissionsArray;
     }
 
     @NotNull
-    private WritableNativeArray toParticipants(@NotNull Conference conference) {
+    private String toString(@NotNull ConferencePermission permission) {
+        switch (permission) {
+            case INVITE:
+                return "Invite";
+            case JOIN:
+                return "Join";
+            case KICK:
+                return "Kick";
+            case RECORD:
+                return "Record";
+            case SEND_AUDIO:
+                return "SendAudio";
+            case SEND_MESSAGE:
+                return "SendMessage";
+            case SEND_VIDEO:
+                return "SendVideo";
+            case SHARE_FILE:
+                return "ShareFile";
+            case SHARE_SCREEN:
+                return "ShareScreen";
+            case SHARE_VIDEO:
+                return "ShareVideo";
+            case STREAM:
+                return "Stream";
+            case UPDATE_PERMISSIONS:
+                return "UpdatePermissions";
+            default:
+                return "Unknown";
+        }
+    }
+
+    @NotNull
+    private WritableNativeArray toParticipantsArray(@NotNull CopyOnWriteArrayList<Participant> participants) {
         WritableNativeArray participantsArray = new WritableNativeArray();
-        for (Participant participant : conference.getParticipants()) {
-            participantsArray.pushMap(participantMapper.toMap(participant));
+        for (Participant participant : participants) {
+            if (participant != null) {
+                participantsArray.pushMap(participantMapper.toMap(participant));
+            }
         }
         return participantsArray;
     }
--- android/src/main/java/io/dolby/sdk/reactnative/mapper/ParticipantMapper.java
+++ android/src/main/java/io/dolby/sdk/reactnative/mapper/ParticipantMapper.java
@@ -9,11 +9,12 @@ import com.facebook.react.bridge.WritableNativeArray;
 import com.facebook.react.bridge.WritableNativeMap;
 import com.voxeet.android.media.MediaStream;
 import com.voxeet.android.media.stream.AudioTrack;
+import com.voxeet.android.media.stream.MediaStreamType;
 import com.voxeet.android.media.stream.VideoTrack;
 import com.voxeet.sdk.json.ParticipantInfo;
 import com.voxeet.sdk.models.Participant;
 import com.voxeet.sdk.models.v1.ConferenceParticipantStatus;
-import com.voxeet.sdk.utils.Opt;
+import com.voxeet.sdk.models.v2.ParticipantType;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -32,7 +33,7 @@ public class ParticipantMapper {
     public final static String PARTICIPANT_INFO_NAME = "name";
     public final static String PARTICIPANT_INFO_EXTERNAL_ID = "externalId";
     public final static String PARTICIPANT_INFO_AVATAR_URL = "avatarUrl";
-    public final static String PARTICIPANT_CONFERENCE_STATUS = "conferenceStatus";
+    public final static String PARTICIPANT_STATUS = "status";
     public final static String PARTICIPANT_STREAMS = "streams";
     public final static String PARTICIPANT_STREAMS_ID = "id";
     public final static String PARTICIPANT_STREAMS_TYPE = "type";
@@ -41,7 +42,7 @@ public class ParticipantMapper {
     public final static String PARTICIPANT_TYPE = "type";
 
     @NonNull
-    public ParticipantInfo toParticipantInfo(ReadableMap map) {
+    public ParticipantInfo toParticipantInfo(@NotNull ReadableMap map) {
         return new ParticipantInfo(
                 map.getString(PARTICIPANT_INFO_NAME),
                 map.getString(PARTICIPANT_INFO_EXTERNAL_ID),
@@ -51,65 +52,113 @@ public class ParticipantMapper {
 
     @NonNull
     public WritableMap toMap(@NonNull Participant participant) {
-        ParticipantInfo participantInfo = participant.getInfo();
-
         WritableMap map = new WritableNativeMap();
-        map.putString(PARTICIPANT_ID, participant.getId());
-        map.putBoolean(PARTICIPANT_AUDIO_TRANSMITTING, participant.audioTransmitting());
-        map.putString(
-                PARTICIPANT_CONFERENCE_STATUS,
-                Opt.of(participant.getStatus()).or(ConferenceParticipantStatus.UNKNOWN).name()
-        );
 
-        map.putMap(PARTICIPANT_INFO, toMap(participantInfo));
-        map.putArray(PARTICIPANT_STREAMS, toStreams(participant));
-        // fixme waiting for answer regarding types other than USER and LISTENER
-        map.putString(PARTICIPANT_TYPE, participant.participantType().name());
+        if (participant.getId() != null) {
+            map.putString(PARTICIPANT_ID, participant.getId());
+        }
+        map.putBoolean(PARTICIPANT_AUDIO_TRANSMITTING, participant.audioTransmitting());
+        map.putString(PARTICIPANT_STATUS, toString(participant.getStatus()));
+        if (participant.getInfo() != null) {
+            map.putMap(PARTICIPANT_INFO, toMap(participant.getInfo()));
+        }
+        map.putArray(PARTICIPANT_STREAMS, toStreamsArray(participant.streams()));
+        map.putString(PARTICIPANT_TYPE, toString((participant.participantType())));
 
         return map;
     }
 
     @NotNull
-    private WritableMap toMap(ParticipantInfo participantInfo) {
+    private String toString(@NotNull ConferenceParticipantStatus participantStatus) {
+        switch (participantStatus) {
+            case CONNECTING:
+                return "CONNECTING";
+            case DECLINE:
+                return "DECLINE";
+            case ERROR:
+                return "ERROR";
+            case INACTIVE:
+                return "INACTIVE";
+            case KICKED:
+                return "KICKED";
+            case LEFT:
+                return "LEFT";
+            case RESERVED:
+                return "RESERVED";
+            case WARNING:
+                return "WARNING";
+            case IN_PROGRESS:
+            case ON_AIR:
+            case LATER:
+            case MISSED:
+            case UNKNOWN:
+            default:
+                return "UNKNOWN";
+        }
+    }
+
+    @NotNull
+    private WritableMap toMap(@NotNull ParticipantInfo participantInfo) {
         WritableMap map = new WritableNativeMap();
 
-        if (participantInfo != null) {
-            if (participantInfo.getName() != null) {
-                map.putString(PARTICIPANT_INFO_NAME, participantInfo.getName());
-            }
-            if (participantInfo.getExternalId() != null) {
-                map.putString(PARTICIPANT_INFO_EXTERNAL_ID, participantInfo.getExternalId());
-            }
-            if (participantInfo.getAvatarUrl() != null) {
-                map.putString(PARTICIPANT_INFO_AVATAR_URL, participantInfo.getAvatarUrl());
-            }
+        if (participantInfo.getName() != null) {
+            map.putString(PARTICIPANT_INFO_NAME, participantInfo.getName());
+        }
+        if (participantInfo.getExternalId() != null) {
+            map.putString(PARTICIPANT_INFO_EXTERNAL_ID, participantInfo.getExternalId());
+        }
+        if (participantInfo.getAvatarUrl() != null) {
+            map.putString(PARTICIPANT_INFO_AVATAR_URL, participantInfo.getAvatarUrl());
         }
 
         return map;
     }
 
     @NotNull
-    private WritableNativeArray toStreams(@NotNull Participant participant) {
+    private WritableNativeArray toStreamsArray(@NotNull List<MediaStream> streams) {
         WritableNativeArray streamsArray = new WritableNativeArray();
-        for (MediaStream stream : participant.streams()) {
-            streamsArray.pushMap(toStream(stream));
+        for (MediaStream stream : streams) {
+            if (stream != null) {
+                streamsArray.pushMap(toMap(stream));
+            }
         }
         return streamsArray;
     }
 
-    private ReadableMap toStream(MediaStream stream) {
+    @NotNull
+    private ReadableMap toMap(@NotNull MediaStream stream) {
         WritableNativeMap map = new WritableNativeMap();
 
         map.putString(PARTICIPANT_STREAMS_ID, stream.peerId());
-        // fixme waiting for answer regarding types other than CAMERA and SCREEN SHARE
-        map.putString(PARTICIPANT_STREAMS_TYPE, stream.getType().name());
-        map.putArray(PARTICIPANT_STREAMS_AUDIO_TRACKS, toAudioTracks(stream.audioTracks()));
-        map.putArray(PARTICIPANT_STREAMS_VIDEO_TRACKS, toVideoTracks(stream.videoTracks()));
+        map.putString(PARTICIPANT_STREAMS_TYPE, toString(stream.getType()));
+        map.putArray(PARTICIPANT_STREAMS_AUDIO_TRACKS, toAudioTrackArray(stream.audioTracks()));
+        map.putArray(PARTICIPANT_STREAMS_VIDEO_TRACKS, toVideoTrackArray(stream.videoTracks()));
 
         return map;
     }
 
-    private ReadableArray toAudioTracks(List<AudioTrack> audioTracks) {
+    @NotNull
+    private String toString(@NotNull MediaStreamType mediaStreamType) {
+        switch (mediaStreamType) {
+            case Camera:
+                return "Camera";
+            case ScreenShare:
+                return "ScreenShare";
+            case Custom1:
+            case Custom2:
+            case Custom3:
+            case Custom4:
+            case Custom5:
+            case Custom6:
+            case Custom7:
+            case Custom8:
+            default:
+                return "Unknown";
+        }
+    }
+
+    @NotNull
+    private ReadableArray toAudioTrackArray(@NotNull List<AudioTrack> audioTracks) {
         WritableNativeArray streamsArray = new WritableNativeArray();
         for (AudioTrack audioTrack : audioTracks) {
             if (audioTrack != null) {
@@ -119,7 +168,8 @@ public class ParticipantMapper {
         return streamsArray;
     }
 
-    private ReadableArray toVideoTracks(List<VideoTrack> videoTracks) {
+    @NotNull
+    private ReadableArray toVideoTrackArray(@NotNull List<VideoTrack> videoTracks) {
         WritableNativeArray streamsArray = new WritableNativeArray();
         for (VideoTrack videoTrack : videoTracks) {
             if (videoTrack != null) {
@@ -127,5 +177,28 @@ public class ParticipantMapper {
             }
         }
         return streamsArray;
+    }
+
+    @NotNull
+    private String toString(@NotNull ParticipantType participantType) {
+        switch (participantType) {
+            case USER:
+                return "USER";
+            case LISTENER:
+                return "LISTENER";
+            case SPEAKER:
+            case PSTN:
+            case MIXER:
+            case NONE:
+            case DVC:
+            case ROBOT:
+            case ROBOT_SPEAKER:
+            case ROBOT_LISTENER:
+            case ROBOT_PSTN:
+            case ROBOT_MIXER:
+            case ROBOT_NONE:
+            default:
+                return "UNKNOWN";
+        }
     }
 }
