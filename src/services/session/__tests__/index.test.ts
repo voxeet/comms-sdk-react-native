4a638f6b700c925f97696df97e492dafcc4d4e6a
Piotrek Polus <piotr.polus@miquido.com>
2021-10-06 12:52:06 +0200

    DEXA-145 getCurrentParticipant

--- src/services/session/__tests__/index.test.ts
+++ src/services/session/__tests__/index.test.ts
@@ -1,25 +1,55 @@
 import SessionService from '../SessionService';
+import { transformToUser } from '../transformers';
 import { NativeModules } from 'react-native';
 
 const { DolbyIoIAPISessionServiceModule } = NativeModules;
 
-/** SessionService tests */
+describe('ConferenceService', () => {
+  describe('open()', () => {
+    it('should invoke exported method', () => {
+      SessionService.open({ name: 'Jack' });
+      expect(DolbyIoIAPISessionServiceModule.open).toHaveBeenCalledWith({
+        name: 'Jack',
+      });
+    });
 
-describe('SessionService', () => {
-  test('Open method calls exported open method', () => {
-    SessionService.open({ name: 'Jack' });
-    expect(DolbyIoIAPISessionServiceModule.open).toHaveBeenCalledWith({
-      name: 'Jack',
+    it('should invoke exported method with empty object when no param passed', () => {
+      SessionService.open();
+      expect(DolbyIoIAPISessionServiceModule.open).toHaveBeenCalledWith({});
     });
   });
 
-  test('Open method without param calls exported open method with empty object', () => {
-    SessionService.open();
-    expect(DolbyIoIAPISessionServiceModule.open).toHaveBeenCalledWith({});
+  describe('close()', () => {
+    it('should invoke exported method', () => {
+      SessionService.close();
+      expect(DolbyIoIAPISessionServiceModule.open).toHaveBeenCalled();
+    });
   });
 
-  test('Close method calls exported close method', () => {
-    SessionService.close();
-    expect(DolbyIoIAPISessionServiceModule.open).toHaveBeenCalled();
+  describe('getsCurrentUser()', () => {
+    it('should invoke exported method', () => {
+      SessionService.getCurrentUser();
+      expect(DolbyIoIAPISessionServiceModule.getParticipant).toHaveBeenCalled();
+    });
+  });
+});
+
+describe('SessionService - transformers', () => {
+  describe('transformToUser()', () => {
+    it('should return User object', () => {
+      expect(
+        transformToUser({
+          info: {
+            name: 'Jack',
+          },
+          id: '111',
+        })
+      ).toStrictEqual({
+        info: {
+          name: 'Jack',
+        },
+        id: '111',
+      });
+    });
   });
 });
