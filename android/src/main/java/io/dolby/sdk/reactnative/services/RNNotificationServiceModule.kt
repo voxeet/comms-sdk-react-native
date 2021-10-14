fd5b129a1f600c8c7fe770320a34ec4f1a819d44
Roman Savka <roman.savka@miquido.com>
2021-10-14 09:31:40 +0200

    DEXA-115 Notification service docs

--- android/src/main/java/io/dolby/sdk/reactnative/services/RNNotificationServiceModule.kt
+++ android/src/main/java/io/dolby/sdk/reactnative/services/RNNotificationServiceModule.kt
@@ -1,6 +1,12 @@
 package io.dolby.sdk.reactnative.services
 
-import com.facebook.react.bridge.*
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableArray
+import com.facebook.react.bridge.ReadableMap
+import com.voxeet.sdk.models.Conference
+import com.voxeet.sdk.models.Participant
 import com.voxeet.sdk.services.ConferenceService
 import com.voxeet.sdk.services.NotificationService
 import io.dolby.sdk.reactnative.mapper.ConferenceMapper
@@ -12,7 +18,18 @@ import io.dolby.sdk.reactnative.utils.Promises.thenValue
 import io.dolby.sdk.reactnative.utils.ReactPromise
 
 /**
+ * The [RNNotificationServiceModule] allows inviting participants to a conference.
  *
+ * The application calls the [invite] method to invite specific participants to a conference.
+ * Participants who do not wish to participate at a conference can [decline] the conference invitation.
+ *
+ * @constructor
+ * Creates a bridge wrapper for [NotificationService].
+ *
+ * @param conferenceService   [ConferenceService] from Android SDK
+ * @param notificationService [NotificationService] from Android SDK
+ * @param conferenceMapper    [ConferenceMapper] mapper for a [Conference] and [Conference]-related models
+ * @param participantMapper   [ParticipantMapper] mapper for a [Participant] and [Participant]-related models
  */
 class RNNotificationServiceModule(
         private val conferenceService: ConferenceService,
@@ -25,7 +42,13 @@ class RNNotificationServiceModule(
     override fun getName(): String = "DolbyIoIAPINotificationService"
 
     /**
+     * Notifies conference participants about a conference invitation.
      *
+     * The ParticipantInfo model included in the invitation has to include externalId.
+     *
+     * @param conferenceMap        a conference to invite
+     * @param participantInfoArray information about the invited users
+     * @param promise              returns null
      */
     @ReactMethod
     fun invite(conferenceMap: ReadableMap, participantInfoArray: ReadableArray, promise: ReactPromise) {
@@ -39,7 +62,10 @@ class RNNotificationServiceModule(
     }
 
     /**
+     * Declines the conference invitation.
      *
+     * @param conferenceMap a conference to decline
+     * @param promise       returns true if decline request succeed, false otherwise
      */
     @ReactMethod
     fun decline(conferenceMap: ReadableMap, promise: ReactPromise) {
