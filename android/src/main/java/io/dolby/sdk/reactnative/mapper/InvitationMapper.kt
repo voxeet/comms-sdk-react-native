cfab506f18a94f420ffb0e7f54df5e760222dc6f
aamq <agnieszka.adamczyk@miquido.com>
2021-10-15 14:38:57 +0200

    DEXA-92 Conference Service - convert module to Kotlin and apply new code style

--- android/src/main/java/io/dolby/sdk/reactnative/mapper/InvitationMapper.kt
+++ android/src/main/java/io/dolby/sdk/reactnative/mapper/InvitationMapper.kt
@@ -5,22 +5,22 @@ import com.facebook.react.bridge.ReadableMap
 import com.voxeet.sdk.json.ParticipantInvited
 
 class InvitationMapper(
-        private val permissionMapper: ConferencePermissionMapper,
-        private val participantMapper: ParticipantMapper
+    private val permissionMapper: ConferencePermissionMapper,
+    private val participantMapper: ParticipantMapper
 ) {
 
-    private companion object {
-        private const val PARTICIPANT_INFO = "info"
-        private const val PARTICIPANT_PERMISSIONS = "permissions"
-    }
+  private companion object {
+    private const val PARTICIPANT_INFO = "info"
+    private const val PARTICIPANT_PERMISSIONS = "permissions"
+  }
 
-    fun decode(invitedParticipantsArray: ReadableArray): List<ParticipantInvited> {
-        return invitedParticipantsArray.toArrayList()
-                .filterIsInstance<ReadableMap>()
-                .mapNotNull { map ->
-                    val info = map.getMap(PARTICIPANT_INFO)?.let(participantMapper::toParticipantInfo)
-                    val permissions = map.getArray(PARTICIPANT_PERMISSIONS)?.let(permissionMapper::decode)
-                    info?.let(::ParticipantInvited)?.apply { setPermissions(permissions) }
-                }
-    }
+  fun decode(invitedParticipantsArray: ReadableArray): List<ParticipantInvited> {
+    return invitedParticipantsArray.toArrayList()
+        .filterIsInstance<ReadableMap>()
+        .mapNotNull { map ->
+          val info = map.getMap(PARTICIPANT_INFO)?.let(participantMapper::toParticipantInfo)
+          val permissions = map.getArray(PARTICIPANT_PERMISSIONS)?.let(permissionMapper::decode)
+          info?.let(::ParticipantInvited)?.apply { setPermissions(permissions) }
+        }
+  }
 }
