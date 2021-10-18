d80af818f4a015ce03c1ada40b1912ffd736aa74
Roman Savka <roman.savka@miquido.com>
2021-10-18 12:52:51 +0200

    Update InvitationMapper conventions

--- android/src/main/java/io/dolby/sdk/reactnative/mapper/InvitationMapper.kt
+++ android/src/main/java/io/dolby/sdk/reactnative/mapper/InvitationMapper.kt
@@ -4,9 +4,12 @@ import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.bridge.ReadableMap
 import com.voxeet.sdk.json.ParticipantInvited
 
+/**
+ * Provides methods that map React Native model to [ParticipantInvited] model
+ */
 class InvitationMapper(
-    private val permissionMapper: ConferencePermissionMapper,
-    private val participantMapper: ParticipantMapper
+  private val permissionMapper: ConferencePermissionMapper,
+  private val participantMapper: ParticipantMapper
 ) {
 
   private companion object {
@@ -14,13 +17,13 @@ class InvitationMapper(
     private const val PARTICIPANT_PERMISSIONS = "permissions"
   }
 
-  fun decode(invitedParticipantsArray: ReadableArray): List<ParticipantInvited> {
+  fun fromNative(invitedParticipantsArray: ReadableArray): List<ParticipantInvited> {
     return invitedParticipantsArray.toArrayList()
-        .filterIsInstance<ReadableMap>()
-        .mapNotNull { map ->
-          val info = map.getMap(PARTICIPANT_INFO)?.let(participantMapper::toParticipantInfo)
-          val permissions = map.getArray(PARTICIPANT_PERMISSIONS)?.let(permissionMapper::fromNative)
-          info?.let(::ParticipantInvited)?.apply { setPermissions(permissions) }
-        }
+      .filterIsInstance<ReadableMap>()
+      .mapNotNull { map ->
+        val info = map.getMap(PARTICIPANT_INFO)?.let(participantMapper::toParticipantInfo)
+        val permissions = map.getArray(PARTICIPANT_PERMISSIONS)?.let(permissionMapper::fromNative)
+        info?.let(::ParticipantInvited)?.apply { setPermissions(permissions) }
+      }
   }
 }
