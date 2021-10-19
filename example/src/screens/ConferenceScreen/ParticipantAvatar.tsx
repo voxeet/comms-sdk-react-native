4efd3b99924901f161576f87cd0c76f4f343569c
Arkadiusz Kryk <40758385+arkadiuszkrykM@users.noreply.github.com>
2021-10-19 14:26:20 +0200

    Dexa 31/conference creation and fetching methods example app adjustment (#71)

--- example/src/screens/ConferenceScreen/ParticipantAvatar.tsx
+++ example/src/screens/ConferenceScreen/ParticipantAvatar.tsx
@@ -1,57 +1,48 @@
 import type { Participant } from '../../../../src/services/conference/models';
-import COLORS from '@constants/colors.constants';
-import Text from '@ui/Text';
 import styles from './ConferenceScreen.style';
+import COLORS from '@constants/colors.constants';
+import MenuOptionsButton from '@ui/MenuOptionsButton';
+import type { Options } from '@ui/MenuOptionsButton/MenuOptionsButton';
+import Space from '@ui/Space';
+import Text from '@ui/Text';
+import { mute, kick } from '@utils/conference.tester';
 import React from 'react';
 import { View } from 'react-native';
-import {
-  Menu,
-  MenuTrigger,
-  MenuOptions,
-  MenuOption,
-} from 'react-native-popup-menu';
-import { mute, kick } from '@utils/conference.tester';
-import Space from '@ui/Space';
 
 const ParticipantAvatar = (participant: Participant) => {
-  const onSelect = async (optionValue: string) => {
-    switch (optionValue) {
-      case 'kick':
+  const options: Options = [
+    {
+      text: 'kick',
+      value: 'kick',
+      onSelect: async () => {
         await kick(participant);
-        return;
-      case 'mute':
+      },
+    },
+    {
+      text: 'mute',
+      value: 'mute',
+      onSelect: async () => {
         await mute(participant, true);
-        return;
-      case 'unmute':
+      },
+    },
+    {
+      text: 'unmute',
+      value: 'unmute',
+      onSelect: async () => {
         await mute(participant, false);
-        return;
-      default:
-        return;
-    }
-  };
+      },
+    },
+  ];
 
   return (
     <Space mr="xs">
-      <Menu onSelect={onSelect}>
-        <MenuTrigger triggerOnLongPress>
-          <View style={styles.participant} key={participant.id}>
-            <Text size="s" color={COLORS.BLACK}>
-              {participant.info.name}
-            </Text>
-          </View>
-        </MenuTrigger>
-        <MenuOptions
-          optionsContainerStyle={styles.optionsContainerStyle}
-          customStyles={{
-            optionWrapper: styles.optionWrapper,
-            optionText: styles.optionText,
-          }}
-        >
-          <MenuOption text="Kick" value="kick" />
-          <MenuOption text="Mute" value="mute" />
-          <MenuOption text="Unmute" value="unmute" />
-        </MenuOptions>
-      </Menu>
+      <MenuOptionsButton options={options}>
+        <View style={styles.participant} key={participant.id}>
+          <Text size="s" color={COLORS.BLACK}>
+            {participant.info.name}
+          </Text>
+        </View>
+      </MenuOptionsButton>
     </Space>
   );
 };
