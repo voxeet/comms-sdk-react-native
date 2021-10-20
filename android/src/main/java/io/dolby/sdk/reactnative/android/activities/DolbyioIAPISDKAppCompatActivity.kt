e725b6896a7390486543a6dd24a7974e7ac7f744
krzysztof-drobek <40163235+krzysztof-drobek@users.noreply.github.com>
2021-10-20 14:26:25 +0200

    DEXA-201 Convert remaining java classes to kotlin (#81)

---
+++ android/src/main/java/io/dolby/sdk/reactnative/android/activities/DolbyioIAPISDKAppCompatActivity.kt
@@ -0,0 +1,32 @@
+package io.dolby.sdk.reactnative.android.activities
+
+import com.facebook.react.ReactActivity
+import com.voxeet.VoxeetSDK
+import org.greenrobot.eventbus.EventBus
+
+/**
+ * VoxeetSDKAppCompatActivity manages the call state for the in call notification
+ *
+ * Note : this class will be changed in future as per native versions of the SDK
+ *
+ * so that it will be shipped by default, next react native versions will just need to be :
+ * - deprecated
+ * - state in the documentation that the MainActivity should override the new class from the SDK
+ */
+open class DolbyioIAPISDKAppCompatActivity : ReactActivity() {
+
+  override fun onResume() {
+    super.onResume()
+    VoxeetSDK.instance().register(this)
+    VoxeetSDK.screenShare().consumeRightsToScreenShare()
+  }
+
+  override fun onPause() {
+    // stop fetching stats if any pending
+    if (!VoxeetSDK.conference().isLive) {
+      VoxeetSDK.localStats().stopAutoFetch()
+    }
+    EventBus.getDefault().unregister(this)
+    super.onPause()
+  }
+}
