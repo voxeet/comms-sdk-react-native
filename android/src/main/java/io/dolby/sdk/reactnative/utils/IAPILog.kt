79fa0c333162cf787f68f577c931f94d06857f3f
krzysztof-drobek <40163235+krzysztof-drobek@users.noreply.github.com>
2021-10-20 14:26:25 +0200

    DEXA-201 Convert remaining java classes to kotlin (#81)

---
+++ android/src/main/java/io/dolby/sdk/reactnative/utils/IAPILog.kt
@@ -0,0 +1,9 @@
+package io.dolby.sdk.reactnative.utils
+
+import android.util.Log
+
+object IAPILog {
+    fun log(tag: String, text: String) {
+        Log.d("IAPILog", ":: $tag // $text")
+    }
+}
