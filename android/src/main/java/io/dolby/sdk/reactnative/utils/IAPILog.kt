29ff717626b684395f8f7634babfad28239b638f
aamq <agnieszka.adamczyk@miquido.com>
2021-10-21 12:39:31 +0200

    Update code style to include code indentation + format code

--- android/src/main/java/io/dolby/sdk/reactnative/utils/IAPILog.kt
+++ android/src/main/java/io/dolby/sdk/reactnative/utils/IAPILog.kt
@@ -3,7 +3,7 @@ package io.dolby.sdk.reactnative.utils
 import android.util.Log
 
 object IAPILog {
-    fun log(tag: String, text: String) {
-        Log.d("IAPILog", ":: $tag // $text")
-    }
+  fun log(tag: String, text: String) {
+    Log.d("IAPILog", ":: $tag // $text")
+  }
 }
