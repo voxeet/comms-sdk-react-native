f5a73f3e0e9bd33d668a71ac93e5e061d13ad097
aamq <agnieszka.adamczyk@miquido.com>
2021-11-02 10:30:36 +0100

    DEXA-243 Video Presentation Service - add events (VideoPresentationStarted/Pause/Play/Seek/Stopped)

--- android/src/main/java/io/dolby/sdk/reactnative/RNDolbyioIAPISdkPackage.kt
+++ android/src/main/java/io/dolby/sdk/reactnative/RNDolbyioIAPISdkPackage.kt
@@ -10,6 +10,7 @@ import io.dolby.sdk.reactnative.eventemitters.RNCommandEventEmitter
 import io.dolby.sdk.reactnative.eventemitters.RNConferenceEventEmitter
 import io.dolby.sdk.reactnative.eventemitters.RNNotificationEventEmitter
 import io.dolby.sdk.reactnative.eventemitters.RNSdkEventEmitter
+import io.dolby.sdk.reactnative.eventemitters.RNVideoPresentationEventEmitter
 import io.dolby.sdk.reactnative.mapper.ConferenceCreateOptionsMapper
 import io.dolby.sdk.reactnative.mapper.ConferenceJoinOptionsMapper
 import io.dolby.sdk.reactnative.mapper.ConferenceMapper
@@ -67,6 +68,12 @@ class RNDolbyioIAPISdkPackage : ReactPackage {
       conferenceService = VoxeetSDK.conference(),
       participantMapper = participantMapper
     )
+    val videoPresentationEventEmitter = RNVideoPresentationEventEmitter(
+      reactContext = reactContext,
+      conferenceService = VoxeetSDK.conference(),
+      videoPresentationService = VoxeetSDK.videoPresentation(),
+      participantMapper = participantMapper
+    )
     return listOf(
       RNDolbyioIAPISdkModule(
         reactContext = reactContext,
@@ -110,6 +117,7 @@ class RNDolbyioIAPISdkPackage : ReactPackage {
       ),
       RNVideoPresentationServiceModule(
         reactContext = reactContext,
+        eventEmitter = videoPresentationEventEmitter,
         videoPresentationService = VoxeetSDK.videoPresentation(),
         videoPresentationMapper = videoParticipantMapper
       ),
