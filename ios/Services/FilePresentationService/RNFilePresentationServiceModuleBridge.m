53f9044c1ff4a71f4559de258e4aa2669f59a957
mateuszglapiak <mat815@gmail.com>
2021-11-03 15:51:12 +0100

    DEXA-222: FilePresentationService - start, stop, events (#135)
    Co-authored-by: Mateusz Glapiak <mateusz.glapiak@gmail.com>

--- ios/Services/FilePresentationService/RNFilePresentationServiceModuleBridge.m
+++ ios/Services/FilePresentationService/RNFilePresentationServiceModuleBridge.m
@@ -10,4 +10,11 @@ RCT_EXTERN_METHOD(convert:(NSDictionary * _Nonnull)file
 				  resolver:(RCTPromiseResolveBlock _Nonnull)resolve
 				  rejecter:(RCTPromiseRejectBlock _Nonnull)reject);
 
+RCT_EXTERN_METHOD(start:(NSDictionary * _Nonnull)file
+				  resolver:(RCTPromiseResolveBlock _Nonnull)resolve
+				  rejecter:(RCTPromiseRejectBlock _Nonnull)reject);
+
+RCT_EXTERN_METHOD(stop:(RCTPromiseResolveBlock _Nonnull)resolve
+				  rejecter:(RCTPromiseRejectBlock _Nonnull)reject);
+
 @end
