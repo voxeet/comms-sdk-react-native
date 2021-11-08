6f2c21b91a94334703f672a4f392fe98ff84dae9
ppolus-miquido <35292541+ppolus-miquido@users.noreply.github.com>
2021-11-08 14:14:15 +0100

    Feature/dexa 282 ios video view (#143)
    * DEXA-282 Add VideoView and VideoViewManager
    
    Co-authored-by: Yuriy Ganushevich <yuriy.ganushevich@miquido.com>

---
+++ ios/VideoView/VideoViewManagerBridge.m
@@ -0,0 +1,26 @@
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTViewManager.h>
+#import <React/RCTEventEmitter.h>
+
+@interface RCT_EXTERN_REMAP_MODULE(DIOVideoView, RNVideoViewManager, RCTViewManager)
+
+RCT_EXPORT_VIEW_PROPERTY(isMirror, BOOL);
+
+RCT_EXPORT_VIEW_PROPERTY(scaleType, NSString);
+
+RCT_EXPORT_VIEW_PROPERTY(onCommandEvent, RCTBubblingEventBlock);
+
+RCT_EXTERN_METHOD(attach:(NSNumber * _Nonnull)reactTag
+									dispatchId:(NSNumber * _Nonnull)dispatchId
+									participantId:(NSString *)participantId
+									streamId:(NSString *)streamId);
+
+RCT_EXTERN_METHOD(detach:(NSNumber * _Nonnull)reactTag
+									dispatchId:(NSNumber * _Nonnull)dispatchId);
+
+RCT_EXTERN_METHOD(isAttached:(NSNumber * _Nonnull)reactTag
+									dispatchId:(NSNumber * _Nonnull)dispatchId);
+
+
+@end
