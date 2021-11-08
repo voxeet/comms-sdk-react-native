a4e220e46c390e22760cfef82ac48a756bf40fa3
mateuszglapiak <mat815@gmail.com>
2021-11-08 14:30:06 +0100

    DEXA-271, DEXA-267, DEXA-265, DEXA-262: MediaDeviceServiceModule - switchCamera, setComfortNoiseLevel, isFrontCamera, getComfortNoiseLevel (#142)
    Co-authored-by: Mateusz Glapiak <mateusz.glapiak@gmail.com>

---
+++ ios/Services/MediaDeviceService/RNMediaDeviceServiceModuleBridge.m
@@ -0,0 +1,19 @@
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_REMAP_MODULE(DolbyIoIAPIMediaDeviceService, RNMediaDeviceServiceModule, NSObject)
+
+RCT_EXTERN_METHOD(switchCamera:(RCTPromiseResolveBlock _Nonnull)resolve
+				  rejecter:(RCTPromiseRejectBlock _Nonnull)reject);
+
+RCT_EXTERN_METHOD(isFrontCamera:(RCTPromiseResolveBlock _Nonnull)resolve
+				  rejecter:(RCTPromiseRejectBlock _Nonnull)reject);
+
+RCT_EXTERN_METHOD(getComfortNoiseLevel:(RCTPromiseResolveBlock _Nonnull)resolve
+				  rejecter:(RCTPromiseRejectBlock _Nonnull)reject);
+
+RCT_EXTERN_METHOD(setComfortNoiseLevel:(NSString * _Nonnull)noiseLevel
+				  resolver:(RCTPromiseResolveBlock _Nonnull)resolve
+				  rejecter:(RCTPromiseRejectBlock _Nonnull)reject);
+
+@end
