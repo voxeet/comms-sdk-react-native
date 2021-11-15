93c57175469b4d2458be9e37805e2871333c24d4
arkadiuszkrykM <arkadiusz.kryk@miquido.com>
2021-11-15 14:17:50 +0100

    clean up and improvement of code

---
+++ example/src/components/FilePresentationHandler/FilePresentationHandler.tsx
@@ -0,0 +1,83 @@
+import React, { useEffect } from 'react';
+import Toast from 'react-native-toast-message';
+
+import DolbyIoIAPI from '@dolbyio/react-native-iapi-sdk';
+
+import type {
+  FileConvertedEventType,
+  FilePresentationChangedEventType,
+} from '../../../../src/services/filePresentation/events';
+import { FilePresentationServiceEventNames } from '../../../../src/services/filePresentation/events';
+
+const FilePresentationHandler: React.FC = () => {
+  const convertedFileEventHandler = (event: FileConvertedEventType) => {
+    console.log('ON FILE CONVERTED\n', JSON.stringify(event, null, 2));
+    Toast.show({
+      type: 'custom',
+      props: {
+        title: 'FILE CONVERTED EVENT DATA',
+        content: JSON.stringify(
+          {
+            fileId: event.fileConverted.id,
+            fileName: event.fileConverted.name,
+          },
+          null,
+          2
+        ),
+      },
+    });
+  };
+
+  const filePresentationChangeEventHandler = async (
+    event: FilePresentationChangedEventType,
+    type?: FilePresentationServiceEventNames
+  ) => {
+    console.log(
+      'ON FILE PRESENTATION CHANGE\n',
+      JSON.stringify(event, null, 2),
+      type
+    );
+    Toast.show({
+      type: 'custom',
+      props: {
+        title: 'FILE PRESENTATION CHANGE EVENT DATA',
+        content: JSON.stringify(
+          {
+            changeEventType: type,
+            fileId: event.filePresentation.id,
+          },
+          null,
+          2
+        ),
+      },
+    });
+    if (
+      type === FilePresentationServiceEventNames.FilePresentationStarted ||
+      type === FilePresentationServiceEventNames.FilePresentationUpdated
+    ) {
+      try {
+        const imageSrc = await DolbyIoIAPI.filePresentation.getImage(0);
+        console.log(imageSrc, 'FILE PRESENTATION GET IMAGE LINK');
+      } catch (e) {
+        console.log('failed to get presented file image source', e);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const fileConvertedUnsubscribeFn =
+      DolbyIoIAPI.filePresentation.onFileConverted(convertedFileEventHandler);
+    const filePresentationChangeUnsubscribeFn =
+      DolbyIoIAPI.filePresentation.onFilePresentationChange(
+        filePresentationChangeEventHandler
+      );
+    return () => {
+      fileConvertedUnsubscribeFn();
+      filePresentationChangeUnsubscribeFn();
+    };
+  }, []);
+
+  return null;
+};
+
+export default FilePresentationHandler;
