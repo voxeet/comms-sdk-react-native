15d3a5fd0a3a02e37ca0e4ac29ca8b714c2fc88b
krzysztof-drobek <40163235+krzysztof-drobek@users.noreply.github.com>
2021-11-18 10:34:43 +0100

    DEXA-268 Fix comfort noise level and participant connected status (#164)

--- android/src/main/java/io/dolby/sdk/reactnative/mapper/MediaMapper.kt
+++ android/src/main/java/io/dolby/sdk/reactnative/mapper/MediaMapper.kt
@@ -1,35 +1,24 @@
 package io.dolby.sdk.reactnative.mapper
 
-import com.facebook.react.bridge.Arguments
-import com.facebook.react.bridge.ReadableMap
 import com.voxeet.android.media.MediaEngine
 
 /**
  * Provides methods that map media-related models to React Native models and vice versa
  */
-class MediaMapper() {
+class MediaMapper {
 
-  fun fromRN(rationaleMap: ReadableMap) = when (rationaleMap.getString(COMFORT_NOISE_LEVEL)) {
-    "DEFAULT" -> MediaEngine.ComfortNoiseLevel.DEFAULT
-    "MEDIUM" -> MediaEngine.ComfortNoiseLevel.MEDIUM
-    "LOW" -> MediaEngine.ComfortNoiseLevel.LOW
-    "OFF" -> MediaEngine.ComfortNoiseLevel.OFF
+  fun fromRN(comfortNoiseLevelRN: String) = when (comfortNoiseLevelRN) {
+    "default" -> MediaEngine.ComfortNoiseLevel.DEFAULT
+    "medium" -> MediaEngine.ComfortNoiseLevel.MEDIUM
+    "low" -> MediaEngine.ComfortNoiseLevel.LOW
+    "off" -> MediaEngine.ComfortNoiseLevel.OFF
     else -> null
   }
 
-  fun toRN(comfortNoiseLevel: MediaEngine.ComfortNoiseLevel): ReadableMap =
-    Arguments
-      .createMap()
-      .apply { putString(COMFORT_NOISE_LEVEL, comfortNoiseLevel.toRnComfortNoiseLevel()) }
-
-  private fun MediaEngine.ComfortNoiseLevel.toRnComfortNoiseLevel() = when (this) {
-    MediaEngine.ComfortNoiseLevel.DEFAULT -> "DEFAULT"
-    MediaEngine.ComfortNoiseLevel.MEDIUM -> "MEDIUM"
-    MediaEngine.ComfortNoiseLevel.LOW -> "LOW"
-    MediaEngine.ComfortNoiseLevel.OFF -> "OFF"
-  }
-
-  private companion object {
-    private const val COMFORT_NOISE_LEVEL = "comfortNoiseLevel"
+  fun toRN(comfortNoiseLevel: MediaEngine.ComfortNoiseLevel) = when (comfortNoiseLevel) {
+    MediaEngine.ComfortNoiseLevel.DEFAULT -> "default"
+    MediaEngine.ComfortNoiseLevel.MEDIUM -> "medium"
+    MediaEngine.ComfortNoiseLevel.LOW -> "low"
+    MediaEngine.ComfortNoiseLevel.OFF -> "off"
   }
 }
