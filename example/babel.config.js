33f0da0cf9ad975e4d8a8c30a7ee3976f5854f1c
Piotrek Polus <piotr.polus@miquido.com>
2021-11-30 18:56:16 +0100

    DEXA-322 Licenses + dotenv removed

--- example/babel.config.js
+++ example/babel.config.js
@@ -19,17 +19,6 @@ module.exports = {
         },
       },
     ],
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-        blocklist: null,
-        allowlist: null,
-        safe: false,
-        allowUndefined: true,
-      },
-    ],
     'react-native-reanimated/plugin',
   ],
 };
