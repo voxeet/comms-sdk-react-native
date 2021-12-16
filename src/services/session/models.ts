31184b75e07852a31fa1e8463cb5d585fe0915f6
Monika <66684637+mhelf-dolby@users.noreply.github.com>
2021-12-16 11:22:36 +0100

    CCDP-867: Additional documentation changes (#199)

--- src/services/session/models.ts
+++ src/services/session/models.ts
@@ -1,6 +1,6 @@
 import type { ParticipantInfo } from '../conference/models';
 
-/**  The User interface gathers information the local participant that belongs to the current session.  */
+/**  The User interface gathers information about the local participant that belongs to the current session.  */
 export interface User {
   /**  The ID of the local participant. */
   id: string;
