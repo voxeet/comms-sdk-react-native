bb65dedbbc5372d8cdbaf1a8ea2cada81716d165
michal.gruszczak <michal.gruszczak@miquido.com>
2021-12-16 16:29:17 +0100

    DEXA-362-rn-additional-renames

--- example/src/components/VideoPresentationHandler/VideoPresentationHandler.tsx
+++ example/src/components/VideoPresentationHandler/VideoPresentationHandler.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import Toast from 'react-native-toast-message';
 
-import CommsAPI from '@dolbyio/react-native-iapi-sdk';
+import CommsAPI from '@dolbyio/comms-sdk-reactnative';
 
 import type {
   VideoPresentationEventType,
