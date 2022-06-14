2bdc4b3d7ca6909a3fc62abad37d61782e9d8e05
Krzysztof Dróbek <krzysztof.drobek@miquido.com>
2022-06-14 16:15:14 +0200

    CCS-2982 Add shared spatial scene param

--- android/src/main/java/io/dolby/sdk/comms/reactnative/mapper/ConferenceCreateOptionsMapper.kt
+++ android/src/main/java/io/dolby/sdk/comms/reactnative/mapper/ConferenceCreateOptionsMapper.kt
@@ -4,9 +4,11 @@ import com.facebook.react.bridge.ReadableMap
 import com.voxeet.sdk.json.internal.MetadataHolder
 import com.voxeet.sdk.json.internal.ParamsHolder
 import com.voxeet.sdk.services.builders.ConferenceCreateOptions
+import com.voxeet.sdk.services.conference.spatialisation.SpatialAudioStyle
 import io.dolby.sdk.comms.reactnative.mapper.ConferenceCommonConstants.CONFERENCE_PARAMS_DOLBY_VOICE
 import io.dolby.sdk.comms.reactnative.mapper.ConferenceCommonConstants.CONFERENCE_PARAMS_LIVE_RECORDING
 import io.dolby.sdk.comms.reactnative.mapper.ConferenceCommonConstants.CONFERENCE_PARAMS_RTCP_MODE
+import io.dolby.sdk.comms.reactnative.mapper.ConferenceCommonConstants.CONFERENCE_PARAMS_SPATIAL_AUDIO_STYLE
 import io.dolby.sdk.comms.reactnative.mapper.ConferenceCommonConstants.CONFERENCE_PARAMS_TTL
 import io.dolby.sdk.comms.reactnative.mapper.ConferenceCommonConstants.CONFERENCE_PARAMS_VIDEO_CODEC
 import io.dolby.sdk.comms.reactnative.utils.RnCollections.getOptionalBoolean
@@ -34,6 +36,8 @@ class ConferenceCreateOptionsMapper {
     this?.getMap(CONFERENCE_CREATE_OPTIONS_PARAMS)?.let { paramsMap ->
       paramsMap.getString(CONFERENCE_PARAMS_VIDEO_CODEC)
         ?.let(paramsHolder::setVideoCodec)
+      paramsMap.getString(CONFERENCE_PARAMS_SPATIAL_AUDIO_STYLE)
+        ?.let { paramsHolder.setSpatialAudioStyle(spatialAudioStyleFromRN(it)) }
       paramsMap.getOptionalInt(CONFERENCE_PARAMS_TTL)
         ?.let { paramsHolder.putValue(CONFERENCE_PARAMS_TTL, it) }
       paramsMap.getString(CONFERENCE_PARAMS_RTCP_MODE)
@@ -42,9 +46,17 @@ class ConferenceCreateOptionsMapper {
         ?.let { paramsHolder.putValue(CONFERENCE_PARAMS_LIVE_RECORDING, it) }
       paramsMap.getOptionalBoolean(CONFERENCE_PARAMS_DOLBY_VOICE)
         ?.let { paramsHolder.putValue(CONFERENCE_PARAMS_DOLBY_VOICE, it) }
+      paramsMap.getString(CONFERENCE_PARAMS_SPATIAL_AUDIO_STYLE)
+        ?.let { paramsHolder.putValue(CONFERENCE_PARAMS_SPATIAL_AUDIO_STYLE, it) }
     }
 
     return paramsHolder
+  }
+
+  private fun spatialAudioStyleFromRN(style: String) = when (style) {
+    "INDIVIDUAL" -> SpatialAudioStyle.INDIVIDUAL
+    "SHARED" -> SpatialAudioStyle.SHARED
+    else -> SpatialAudioStyle.DISABLED
   }
 
   companion object {
