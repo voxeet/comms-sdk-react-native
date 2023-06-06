44a8d8af459ce4aa494ccab10911c92d799b82b0
Monika <monika.helfer@dolby.com>
2023-06-06 15:28:13 +0200

    Added the Authentication v2 changes

--- src/services/conference/models.ts
+++ src/services/conference/models.ts
@@ -175,7 +175,8 @@ export interface Participant {
 export interface ParticipantInfo {
   /** The URL of the participant's avatar. */
   avatarUrl?: string;
-  /** The external unique identifier that the customer's application can add to the participant while opening a session. If a participant uses the same external ID in conferences, the participant's ID also remains the same across all sessions. This property is required to invite participants to a conference.
+  /** The external unique identifier that a customer's application can add to a participant when applying for a [client access token](ref:get-client-access-token). Setting the external ID via the [open](doc:rn-client-sdk-references-sessionservice#open) method is no longer supported.
+   * If a participant uses the same external ID in conferences, the participant's ID also remains the same across all sessions.
    */
   externalId?: string;
   /** The participant's name. */
