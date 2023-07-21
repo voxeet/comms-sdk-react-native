3ac75e6e3c4c7cfb038435bd950f524acebc1745
mpozy <100850233+mpozy@users.noreply.github.com>
2023-07-21 09:18:41 +0200

    Add message alert (#232)

--- example/src/components/MessageHandler/MessageHandler.tsx
+++ example/src/components/MessageHandler/MessageHandler.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-
+import { Alert } from 'react-native';
 import CommsAPI from '@dolbyio/comms-sdk-react-native';
 
 import type { MessageReceivedEventType } from '@dolbyio/comms-sdk-react-native/models';
@@ -9,6 +9,17 @@ const MessageHandler: React.FC = () => {
     console.log(
       'MESSAGE RECEIVED EVENT DATA: \n',
       JSON.stringify(data, null, 2)
+    );
+    const {
+      message,
+      participant: {
+        info: { name },
+      },
+    } = data;
+
+    Alert.alert(
+      'MESSAGE RECEIVED',
+      JSON.stringify({ participant: name, message: message }, null, 2),
     );
   };
 
